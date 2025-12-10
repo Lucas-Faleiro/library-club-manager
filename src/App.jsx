640daef44ca6043a9fff98146fcf4a6c505c3f45
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2025-12-10 17:10:03 -0300

    Exercício 5: Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import ClubeLista from "./components/ClubeLista";
+import NovoClube from "./components/NovoClube";
 
 function App() {
   return (
-    <div>
-      <ClubeLista />
-    </div>
+    <Routes>
+      <Route index element={<ClubeLista />} />
+      <Route path="/adicionar" element={<NovoClube />} />
+    </Routes>
   );
 }
 
